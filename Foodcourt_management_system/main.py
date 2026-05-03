2d3c90f2f72b73ac3c97c4d242ee71f083f0d4ab
zeditya <adityabahuguna098@gmail.com>
2026-05-03 23:14:05 +0530

    Add Matplotlib graph and MongoDB update functionality

--- Foodcourt_management_system/main.py
+++ Foodcourt_management_system/main.py
@@ -1,7 +1,8 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox,simpledialog
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from mydb import start_db
 
 cart=[]
@@ -32,9 +33,31 @@ def bill():
     txt+="\nFinal:"+str(fin)
     messagebox.showinfo("Bill",txt)
 
+def update():
+    try:
+        sel=box.get(box.curselection())
+        n=sel.split("-")[0].strip()
+        new_p=simpledialog.askinteger("Price","New price:")
+        if new_p:
+            db=start_db()
+            db["food_items"].update_one({"name":n},{"$set":{"price":new_p}})
+            messagebox.showinfo("Done","Updated! Restart app")
+    except:
+        messagebox.showerror("Error","Select item")
+
+def graph():
+    if len(cart)==0:
+        messagebox.showwarning("Wait","Cart empty")
+        return
+    df=pd.DataFrame(cart)
+    cnt=df["Item"].value_counts()
+    plt.bar(cnt.index,cnt.values)
+    plt.title("Items in Cart")
+    plt.show()
+
 win=tk.Tk()
-win.title("Food court management system")
-win.geometry("300x400")
+win.title("Food Project")
+win.geometry("300x500")
 
 l1=tk.Label(win,text="Menu")
 l1.pack()
@@ -47,9 +70,17 @@ data=db["food_items"].find()
 
 for x in data:
     box.insert("end",x["name"]+"-"+str(x["price"])+"Rs")
+
 b1=tk.Button(win,text="Add",command=add)
 b1.pack()
+
 b2=tk.Button(win,text="Bill",command=bill)
 b2.pack()
+
+b3=tk.Button(win,text="Update",command=update)
+b3.pack()
+
+b4=tk.Button(win,text="Graph",command=graph)
+b4.pack()
 
 win.mainloop()
